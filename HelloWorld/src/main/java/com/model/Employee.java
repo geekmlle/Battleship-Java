f211d250e8c4b6790d0fac738467e8f7d94d8167
Diana Michelle <geekmlle@gmail.com>
2016-02-21 13:35:56 -0500

    LOL
    LOL

---
+++ HelloWorld/src/main/java/com/model/Employee.java
@@ -0,0 +1,32 @@
+package com.model;
+
+/**
+ * Created by blondieymollo on 2/21/16.
+ */
+public class Employee {
+
+    private int id;
+    private String name;
+    private String role;
+    public Employee() {
+    }
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getRole() {
+        return role;
+    }
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+}
